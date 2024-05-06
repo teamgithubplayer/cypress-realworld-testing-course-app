dcd46007002a0e5a1064eac8bbec57bb6ebc052a
henry mutua <mutuahenry116@gmail.com>
2024-05-06 12:09:36 +0300

    Home spec file

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,16 @@
+describe('Home page', () => {
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000')
+  })
+
+  it.only('h1 contains text', () => {
+   
+    cy.getByData("hero-heading").contains('Testing Next.js Applications with Cypress')
+  })
+
+  it('the features on the home page are correct', ()=>{
+ 
+    cy.get('dt').eq(0).contains('4 Courses')
+
+  })
+})
