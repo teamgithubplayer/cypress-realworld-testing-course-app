728b9f9c8d9d1cde0dbb06af949b069b9193838f
Muhammed Naser <muhammednaser@Muhammeds-MacBook-Pro.local>
2024-09-14 15:42:06 -0400

    added method to test dt tag only

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,11 @@
 describe('homepage', () => {
   it('the h1 contains the correct text', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('[data-test="hero-heading"]').contains("Testing Next.js Applications with Cypress")
   })
+})
+
+it.only("the features on the homepage are correct", () => {
+  cy.visit("http://localhost:3000")
+  cy.get("dt")
 })
