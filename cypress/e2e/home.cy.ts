559b1ec5123db1fa3913c0f57a24ec95b05b37c0
Muhammed Naser <muhammednaser@Muhammeds-MacBook-Pro.local>
2024-09-14 22:24:07 -0400

    added context function in home.cy.ts page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,16 +5,19 @@ describe('homepage', () => {
  })
 
 
- it('the h1 contains the correct text', () => {
-  cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+context("Hero Section", ()=>{
+  it('the h1 contains the correct text', () => {
+    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+  })
+  
+  it("the features on the homepage are correct", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  
+  })
+  })
 })
 
-it("the features on the homepage are correct", () => {
-  cy.get("dt").eq(0).contains("4 Courses")
-  cy.get("dt").eq(1).contains("25+ Lessons")
-  cy.get("dt").eq(2).contains("Free and Open Source")
-
-})
-})
 
 
