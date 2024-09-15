d5341186f80cbc998c78ced978e2e0ea4621e141
Muhammed Naser <muhammednaser@Muhammeds-MacBook-Pro.local>
2024-09-15 12:30:16 -0400

    completed user journey courses method test in user-journet.cy.ts

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -13,8 +13,13 @@ describe('User-Journey', () => {
             cy.location("pathname").should("equal", "/testing-your-first-application/app-install-and-overview")
             cy.getByData("challenge-answer-0").click()
             cy.getByData("next-lesson-button").click()
-            
-
+            cy.location("pathname").should("equal", "/testing-your-first-application/installing-cypress-and-writing-our-first-test")
+            cy.getByData("challenge-answer-0").click()
+            cy.getByData("next-lesson-button").click()
+            cy.location("pathname").should("equal", "/testing-your-first-application/setting-up-data-before-each-test")
+            cy.getByData("challenge-answer-0").click()
+            cy.getByData("next-lesson-button").click()
+            cy.location("pathname").should("equal", '/')
         })
         
 
